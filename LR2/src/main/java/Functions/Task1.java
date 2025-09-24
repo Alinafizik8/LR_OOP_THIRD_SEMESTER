6c498753d34f11bc6fee27ac83bd427b0ca87520
Yoda_bl <rnrinat1234@gmail.com>
2025-09-24 19:45:31 +0400

    rename2

--- LR2/src/main/java/Functions/Task1.java
+++ LR2/src/main/java/Functions/Task1.java
@@ -1,4 +1,4 @@
-package Functions;
+package functions;
 
 // Интерфейс MathFunction
 interface MathFunction {
--- LR2/src/test/java/Functions/RungeKuttaFunctionTest.java
+++ LR2/src/test/java/Functions/RungeKuttaFunctionTest.java
@@ -1,4 +1,4 @@
-package Functions;
+package functions;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
