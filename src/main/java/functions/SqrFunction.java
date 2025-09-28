d2c82f7596f72a6b27bed4991080a1cf538f4027
Alina Sergunina <sergunina414@mail.ru>
2025-09-28 19:41:11 +0300

    F-Y-F(first ex - Y person - first line)

--- src/main/java/functions/SqrFunction.java
+++ src/main/java/functions/SqrFunction.java
@@ -1,7 +1,8 @@
 package functions;
 
+//return x^2
 class SqrFunction implements MathFunction {
-
+    private double digit;
     @Override
     public double apply(double x){
         return java.lang.Math.pow(x,2);
--- src/test/java/functions/SqrFunctionTest.java
+++ src/test/java/functions/SqrFunctionTest.java
@@ -5,8 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SqrFunctionTest {
-
     @Test
     void apply() {
+        SqrFunction digit = new SqrFunction();
+        assertEquals(16, digit.apply(4),"4^2=16");
     }
 }
