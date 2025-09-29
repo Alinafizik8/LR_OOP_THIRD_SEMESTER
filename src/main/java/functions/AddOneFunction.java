453ed6c82a10c2e6facf3f7becc99367ed609d52
Yoda_bl <rnrinat1234@gmail.com>
2025-09-29 13:23:44 +0400

    Cleaning

--- src/main/java/functions/AddOneFunction.java
+++
@@ -1,9 +0,0 @@
-package functions;
-
-// f(x) = x + 1
-class AddOneFunction implements MathFunction {
-    @Override
-    public double apply(double x) {
-        return x + 1;
-    }
-}
--- src/main/java/functions/DoubleFunction.java
+++
@@ -1,9 +0,0 @@
-package functions;
-
-// f(x) = 2 * x
-class DoubleFunction implements MathFunction {
-    @Override
-    public double apply(double x) {
-        return 2 * x;
-    }
-}
