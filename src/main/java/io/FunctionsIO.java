7751cf29e5af01c58881a9730ed7e8902f5e68a8
Alina Sergunina <sergunina414@mail.ru>
2025-10-11 22:16:31 +0300

    half of third ex in third task of third lab

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -1,9 +1,25 @@
 package io;
+import functions.*;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
 
 public final class FunctionsIO {
 
     private FunctionsIO() {
         throw new UnsupportedOperationException("Utility class FunctionsIO cannot be instantiated");
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws java.io.IOException {
+
+        PrintWriter printWriter = new PrintWriter(writer);
+        int count = function.getCount();
+        printWriter.println(count);
+
+        for (var point : function) {
+            printWriter.printf("%f %f%n", point.x, point.y);
+        }
+        printWriter.flush();
+
     }
 
 }
