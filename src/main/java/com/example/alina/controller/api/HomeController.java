c4227e21d484568a23cec7a9d0a30ef57461357e
Alina Sergunina <sergunina414@mail.ru>
2025-12-01 13:34:55 +0300

    donerep

---
+++ src/main/java/com/example/alina/controller/api/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.alina.controller.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to LR OOP Spring Boot App! 🚀";
+    }
+}
